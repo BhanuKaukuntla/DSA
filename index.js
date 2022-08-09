d0d2ac3822c1640137417f6cfa7bc5dda507db32
BhanuKaukuntla <kaukuntlabhanu@gmail.com>
2022-08-09 10:21:16 +0530

    commit changes

--- index.js
+++ index.js
@@ -13,3 +13,45 @@ const twoSum = (nums, target) => {
         }
     }
   };
+
+// Longest Substring Without Repeating Characters
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+var lengthOfLongestSubstring = function(s) {
+    let maxLen = 0
+    let start = 0
+    let n = s.length
+    let map = new Map()
+    for(let i=0;i<n;i++){
+        if(map.has(s[i])){
+            start = Math.max(map.get(s[i]) + 1, start)
+        }
+        map.set(s[i], i)
+        maxLen = Math.max(i - start + 1, maxLen)
+    }
+    return maxLen
+};
+
+
+// Container With Most Water
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+
+ var maxArea = function(height) {
+    let area = 0
+    let maxArea = 0
+    let left = 0
+    let right = height.length-1
+    while(left < right){
+        area = Math.min(height[left], height[right]) * (right - left)
+        maxArea = Math.max(area, maxArea)
+        if(height[left] > height[right]){
+            right--
+        }else{
+            left++
+        }
+    }
+    return maxArea
+};
