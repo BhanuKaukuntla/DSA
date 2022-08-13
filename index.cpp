63f25b7b24d72a7850c9cb1ebc7b322368819d36
BhanuKaukuntla <kaukuntlabhanu@gmail.com>
2022-08-13 16:35:06 +0530

    added code

---
+++ index.cpp
@@ -0,0 +1,50 @@
+// Reverse Integer
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while(x) {
+            res = res*10 + x%10;
+            x /= 10;
+        }
+        return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+    }
+};
+
+
+
+// String to Integer (atoi)
+// Input: s = "   -42"
+// Output: -42
+
+
+int myAtoi(string str) {
+    long result = 0;
+    int indicator = 1;
+    for(int i = 0; i<str.size();)
+    {
+        i = str.find_first_not_of(' ');
+        if(str[i] == '-' || str[i] == '+')
+            indicator = (str[i++] == '-')? -1 : 1;
+        while('0'<= str[i] && str[i] <= '9') 
+        {
+            result = result*10 + (str[i++]-'0');
+            if(result*indicator >= INT_MAX) return INT_MAX;
+            if(result*indicator <= INT_MIN) return INT_MIN;                
+        }
+        return result*indicator;
+    }
+}
