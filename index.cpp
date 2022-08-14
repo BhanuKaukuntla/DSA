79dae60fac0c39733912269814d4a1c1a8462bc0
BhanuKaukuntla <kaukuntlabhanu@gmail.com>
2022-08-14 20:30:07 +0530

    commit changes

--- index.cpp
+++ index.cpp
@@ -48,3 +48,47 @@ int myAtoi(string str) {
         return result*indicator;
     }
 }
+
+
+
+// Longest Palindromic Substring
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string lPS(string s){
+    int n = s.size();
+    vector<vector<int>>dp(n, vector<int>(n,0));
+    int maxLength = 0;
+    string ans;
+    for(int diff = 0;diff<n;diff++){
+        for(int i=0, j=i+diff; j<n;i++,j++){
+            if(i==j){
+                dp[i][j]=1;
+            }else if(diff == 1){
+                dp[i][j] = s[i] == s[j] ? 2 : 0;
+            }else{
+                if(s[i] == s[j] && dp[i+1][j-1]){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+            }
+            if(dp[i][j]){
+                if(j-i+1>maxLength){
+                    maxLength = j-i+1;
+                    ans = s.substr(i, maxLength);
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main() {
+    // Write C++ code here
+    std::cout << lPS("babad") ;
+
+    return 0;
+}
