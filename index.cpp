0c2c56ef56c69447a83da881557ac844e19d4838
BhanuKaukuntla <kaukuntlabhanu@gmail.com>
2022-08-21 17:30:06 +0530

    added

--- index.cpp
+++ index.cpp
@@ -298,3 +298,55 @@ int main() {
     }
     return 0;
 }
+
+
+
+// Count and Say
+// Input: n = 4
+// Output: "1211"
+
+string countAndSay(int n, string s = "1") { // initial value of the string is "1"
+    if(n==1) return s; // base case, when n recursive calls are finished
+	
+    int i=0, j, len = s.size(); // 'len' is the length of the string 's'
+    string res = ""; // result after we 'say' the string 's'
+	
+	// 'i' starts from index 0
+    while(i<len) {
+        j=i; // fix 'j' as the current position (i)
+		
+		// increment the value of 'i' till valid position
+        while(i<len && s[i]==s[j]) i++;
+		
+		// substring with same values as s[j] is [j,i-1] & "i-j" is the count of that value
+		// add the concatenation of (count + s[j]) to the result 
+        res += to_string(i-j) + s[j];
+    }
+	
+	// recursively, call the same function by decrementing the count
+	// and update the result 'res', which will be the next string 's'
+    return countAndSay(n-1, res);
+}
+
+
+
+First Missing Positive
+Input: nums = [3,4,-1,1]
+Output: 2
+
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for(int i=0; i<nums.size(); i++){
+            if(i+1==nums[i]) continue;
+            int x = nums[i];
+            while(x>=1 && x<=nums.size() && x!=nums[x-1]){
+                swap(x, nums[x-1]);
+            }
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(i+1!=nums[i])    return i+1;
+        }
+        return nums.size()+1;
+    }
+};
