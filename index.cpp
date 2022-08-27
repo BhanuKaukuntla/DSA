a1c3f5150e385aa7b4ae3829ee1db9adbf13d806
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-27 19:20:32 +0530

    updated

--- index.cpp
+++ index.cpp
@@ -484,3 +484,30 @@ int main() {
     cout << endl;
   }
 }
+
+
+// 15
+// Rotate Image
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& M) {
+        int n = M.size();
+        
+        //First transpose the matrix once!
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+               swap(M[i][j], M[j][i]);
+            }
+        }
+        
+        //Reverse every column!
+        for(int i=0; i<n; i++){
+            reverse(M[i].begin(), M[i].end());
+        }
+    }
+};
+
