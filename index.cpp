ceb6b2cc10a24f7aa7e1944dd7cb9088cec115bf
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-27 22:45:12 +0530

    updated...

--- index.cpp
+++ index.cpp
@@ -568,3 +568,26 @@ double myPow(double x, int n) {
 int main() {
   cout << myPow(2, 10) << endl;
 }
+
+
+
+// 18
+// Maximum Subarray
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, smax = INT_MIN;
+        for (int num : nums) {
+            sum += num;
+            smax = max(smax, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return smax;
+    }
+};
