fd77ccc6ef0a5fd89b83786082f249dfd825de71
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-27 22:28:41 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -545,28 +545,26 @@ public:
 // Output: 1024.00000
 
 
-class Solution {
-public:
-    double myPow(double x, int n) {
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
-        }
-        
-        return pow;
-    }
-};
+#include<bits/stdc++.h>
 
+using namespace std;
+double myPow(double x, int n) {
+  double ans = 1.0;
+  long long nn = n;
+  if (nn < 0) nn = -1 * nn;
+  while (nn) {
+    if (nn % 2) {
+      ans = ans * x;
+      nn = nn - 1;
+    } else {
+      x = x * x;
+      nn = nn / 2;
+    }
+  }
+  if (n < 0) ans = (double)(1.0) / (double)(ans);
+  return ans;
+}
+
+int main() {
+  cout << myPow(2, 10) << endl;
+}
