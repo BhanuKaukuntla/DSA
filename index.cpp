9b1eb8df219895a8cded68ecbca2f35370a52a49
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-28 12:19:44 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -842,3 +842,39 @@ void setZeroes(vector<vector<int> > &matrix) {
         if (col0 == 0) matrix[i][0] = 0;
     }
 }
+
+
+// 27
+// Sort Colors
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0; 
+        int hi = nums.size() - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+         
+    }
+};
