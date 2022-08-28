5f267b8c31682c03e8fc60248239c88f1a1a6f7b
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-28 11:54:17 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -785,3 +785,35 @@ public:
         return r;
     }
 };
+
+
+
+// 25
+// Climbing Stairs
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
