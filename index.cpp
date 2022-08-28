3c7ff5324187f8fee69f7c84981f5470edcf39db
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-28 21:37:10 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -896,3 +896,29 @@ public:
         }
     }
 };
+
+
+// 29
+// Subsets
+// Input: nums = [1,2,3]
+// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs;
+        vector<int> sub;
+        subsets(nums, 0, sub, subs);
+        return subs;
+    }
+private:
+    void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
+        subs.push_back(sub);
+        for (int j = i; j < nums.size(); j++) {
+            sub.push_back(nums[j]);
+            subsets(nums, j + 1, sub, subs);
+            sub.pop_back();
+        }
+    }
+};
