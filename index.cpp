2517918e02d0422745643984adc09448abe54aeb
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-28 11:01:16 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -727,3 +727,31 @@ public:
         return dp[m - 1][n - 1];
     }
 };
+
+
+
+
+// 23
+// Plus One
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size() - 1;i >= 0;i--){
+            if(digits[i] != 9){
+                digits[i]++;
+                return digits;
+            }
+            else
+                digits[i] = 0;
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
