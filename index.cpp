f545af2bd546a882b8943250a1e0f99c45ad0879
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-08-28 10:54:00 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -705,3 +705,25 @@ int main() {
     cout << it[0] << " " << it[1] << "\n";
   }
 }
+
+// 22
+// Unique Paths
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
