b06f5c752648062eec8f889d0f66a1a1ad158891
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-10 18:53:57 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -32,23 +32,34 @@ public:
 // Output: -42
 
 
-int myAtoi(string str) {
-    long result = 0;
-    int indicator = 1;
-    for(int i = 0; i<str.size();)
+class Solution {
+public:
+    int myAtoi(string str)
+{
+    //Your code here
+    int result=0;   //Stores and returns the integer converted value for str
+    int i=0;    //just a current character pointer for string
+    int sign = 1;   //multiplied at the end to result to determine if the string is +ve or -ve
+    i = str.find_first_not_of(' ');
+    if(str[i]=='-') //Check the sign of string (+ or -)
     {
-        i = str.find_first_not_of(' ');
-        if(str[i] == '-' || str[i] == '+')
-            indicator = (str[i++] == '-')? -1 : 1;
-        while('0'<= str[i] && str[i] <= '9') 
-        {
-            result = result*10 + (str[i++]-'0');
-            if(result*indicator >= INT_MAX) return INT_MAX;
-            if(result*indicator <= INT_MIN) return INT_MIN;                
-        }
-        return result*indicator;
+        sign = -1;
+        i++;
     }
+    //Now traverse the entire string and convert it into integer
+    while(str[i])
+    {
+        //If any character is not an integer then just return -1
+        
+        if(!(str[i]-'0'>=0 && str[i]-'0'<=9))
+            return -1;
+        //else just keep updating the result
+        result = result*10 + str[i]-'0';
+        i++;
+    }
+    return result*sign;
 }
+};
 
 
 // 3
