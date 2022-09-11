d70098dca02df44174f99eb3f62868b3446e0459
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-11 22:45:25 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -938,3 +938,22 @@ public:
         return {};
     }
 };
+
+
+//31
+// Longest Substring Without Repeating Characters
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
