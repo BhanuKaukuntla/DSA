8e248d19b04d09552030f55d54d80de911958909
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-12 22:07:03 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -957,3 +957,36 @@ int lengthOfLongestSubstring(string s) {
         }
         return maxLen;
     }
+
+
+//32 
+// Container With Most Water
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxArea(vector<int> &nums)
+{
+    int i = 0,j = nums.size() - 1, mx = INT_MIN;
+	while(i < j)
+	{
+		int water = (j-i)*min(nums[i],nums[j]);
+		mx = max(mx,water);
+		if(nums[i] < nums[j])
+		    i++;
+		else
+		    j--;
+	}
+	
+	return mx;
+}
+
+int main() {
+    vector<int> nums {1, 4, 2, 3};
+  
+    cout << "Maximum amount of water is: " << maxArea(nums);
+  
+    return 0;
+}
