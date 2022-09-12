a408955be257f965d7b6e49f3ddc5cb7b46ff56b
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-12 22:31:52 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1026,3 +1026,63 @@ int romanToInt(string s)
    
    return sum;
 }
+
+
+//34
+// 3Sum
+// Input: nums = [-1,0,1,2,-1,-4]
+// Output: [[-1,-1,2],[-1,0,1]]
+// Explanation: 
+// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+// The distinct triplets are [-1,0,1] and [-1,-1,2].
+// Notice that the order of the output and the order of the triplets does not matter.
+
+#include<bits/stdc++.h>
+using namespace std;
+ vector<vector<int>> threeSum(vector<int>& num) {
+        vector<vector<int>> res; 
+        sort(num.begin(), num.end()); 
+        
+        // moves for a
+        for (int i = 0; i < (int)(num.size())-2; i++) {
+            
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                
+                int lo = i+1, hi = (int)(num.size())-1, sum = 0 - num[i];
+                
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        
+                        vector<int> temp; 
+                        temp.push_back(num[i]); 
+                        temp.push_back(num[lo]); 
+                        temp.push_back(num[hi]); 
+                        res.push_back(temp);
+                        
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        
+                        lo++; hi--;
+                    } 
+                    else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
+            }
+        }
+        return res;
+    }
+int main() {
+  vector < int > arr{-1,0,1,2,-1,-4};
+  vector < vector < int >> ans;
+  ans = threeSum(arr);
+  cout << "The triplets are as follows: "<< endl;
+  for (int i = 0; i < ans.size(); i++) {
+    for (int j = 0; j < ans[i].size(); j++) {
+      cout << ans[i][j] <<" ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
