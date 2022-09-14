169feb06b5bf73998f4bb6b0e23c3f7399757ded
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-14 21:59:19 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1223,3 +1223,32 @@ public:
         return candidate;
     }
 };
+
+
+// 39
+// Contains Duplicate
+// Input: nums = [1,2,3,1]
+// Output: true
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int findDuplicate(vector < int > & nums) {
+  int slow = nums[0];
+  int fast = nums[0];
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+  fast = nums[0];
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+  return slow;
+}
+int main() {
+  vector < int > arr;
+  arr = {1,3,4,2,3};
+  cout << "The duplicate element is " << findDuplicate(arr) << endl;
+}
