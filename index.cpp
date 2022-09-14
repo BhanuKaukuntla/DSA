82395e1e35c953ab1d9505ddf071ef4f866dbd45
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-14 23:02:57 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1337,3 +1337,179 @@ int main() {
   int arr[] ={ 1,2,0,1,0,4,0};
   zerosToEnd(arr, 7);
 }
+
+//42
+// Javascript program to add two numbers
+// represented by linked list
+// Input: l1 = [2,4,3], l2 = [5,6,4]
+// Output: [7,0,8]
+// Explanation: 342 + 465 = 807.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(); 
+        ListNode *temp = dummy; 
+        int carry = 0;
+        while( (l1 != NULL || l2 != NULL) || carry) {
+            int sum = 0; 
+            if(l1 != NULL) {
+                sum += l1->val; 
+                l1 = l1 -> next; 
+            }
+            
+            if(l2 != NULL) {
+                sum += l2 -> val; 
+                l2 = l2 -> next; 
+            }
+            
+            sum += carry; 
+            carry = sum / 10; 
+            ListNode *node = new ListNode(sum % 10); 
+            temp -> next = node; 
+            temp = temp -> next; 
+        }
+        return dummy -> next; 
+    }
+};
+
+
+// 43
+// Binary Tree Inorder Traversal
+// Input: root = [1,null,2,3]
+// Output: [1,3,2]
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node * left, * right;
+};
+
+void inOrderTrav(node * curr, vector < int > & inOrder) {
+  if (curr == NULL)
+    return;
+
+  inOrderTrav(curr -> left, inOrder);
+  inOrder.push_back(curr -> data);
+  inOrderTrav(curr -> right, inOrder);
+}
+
+struct node * newNode(int data) {
+  struct node * node = (struct node * ) malloc(sizeof(struct node));
+  node -> data = data;
+  node -> left = NULL;
+  node -> right = NULL;
+
+  return (node);
+}
+
+int main() {
+
+  struct node * root = newNode(1);
+  root -> left = newNode(2);
+  root -> right = newNode(3);
+  root -> left -> left = newNode(4);
+  root -> left -> right = newNode(5);
+  root -> left -> right -> left = newNode(8);
+  root -> right -> left = newNode(6);
+  root -> right -> right = newNode(7);
+  root -> right -> right -> left = newNode(9);
+  root -> right -> right -> right = newNode(10);
+
+  vector < int > inOrder;
+  inOrderTrav(root, inOrder);
+
+  cout << "The inOrder Traversal is : ";
+  for (int i = 0; i < inOrder.size(); i++) {
+    cout << inOrder[i] << " ";
+  }
+  return 0;
+}
+
+
+
+// 44
+// Remove Nth Node From End of List
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode * start = new ListNode();
+        start -> next = head;
+        ListNode* fast = start;
+        ListNode* slow = start;     
+
+        for(int i = 1; i <= n; ++i)
+            fast = fast->next;
+    
+        while(fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return start->next;
+    }
+};
+
+// 45
+// Median of Two Sorted Arrays
+// Input: nums1 = [1,3], nums2 = [2]
+// Output: 2.00000
+
+#include<bits/stdc++.h>
+using namespace std;
+
+float median(int num 1[],int num2[],int m,int n) {
+    if(m>n)
+        return median(nums2,nums1,n,m);//ensuring that binary search happens on minimum size array
+        
+    int low=0,high=m,medianPos=((m+n)+1)/2;
+    while(low<=high) {
+        int cut1 = (low+high)>>1;
+        int cut2 = medianPos - cut1;
+        
+        int l1 = (cut1 == 0)? INT_MIN:nums1[cut1-1];
+        int l2 = (cut2 == 0)? INT_MIN:nums2[cut2-1];
+        int r1 = (cut1 == m)? INT_MAX:nums1[cut1];
+        int r2 = (cut2 == n)? INT_MAX:nums2[cut2];
+        
+        if(l1<=r2 && l2<=r1) {
+            if((m+n)%2 != 0)
+                return max(l1,l2);
+            else 
+                return (max(l1,l2)+min(r1,r2))/2.0;
+        }
+        else if(l1>r2) high = cut1-1;
+        else low = cut1+1;
+    }
+    return 0.0;
+}
+
+int main() {
+    int nums1[] = {1,4,7,10,12};
+    int nums2[] = {2,3,6,15};
+    int m = sizeof(nums1)/sizeof(nums1[0]);
+    int n = sizeof(nums2)/sizeof(nums2[0]);
+    cout<<"The Median of two sorted arrays is"<<fixed<<setprecision(5)
+    <<median(nums1,nums2,m,n);
+    return 0;
+}
