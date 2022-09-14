405ca4d96d1793248091c1e198e5a34f0a8a7f48
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-14 22:50:05 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1290,3 +1290,50 @@ vector<int>missing_repeated_number(const vector<int> &A) {
 
     return ans;
 }
+
+
+// 41
+// Move Zeroes
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+#include<bits/stdc++.h>
+
+using namespace std;
+void zerosToEnd(int arr[], int n) {
+
+  //finding first zero occurrence
+  int k = 0;
+  while (k < n) {
+    if (arr[k] == 0) {
+
+      break;
+    } else {
+      k = k + 1;
+    }
+  }
+
+  //finding zeros and immediate non-zero elements and swapping them
+  int i = k, j = k + 1;
+
+  while (i < n && j < n) {
+    if (arr[j] != 0) {
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      i++;
+
+    }
+
+    j++;
+
+  }
+  for (i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
+
+int main() {
+  int arr[] ={ 1,2,0,1,0,4,0};
+  zerosToEnd(arr, 7);
+}
