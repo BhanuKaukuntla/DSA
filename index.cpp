99774495f4ed7bb830a7da4cfe46770e156ff2aa
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-14 23:04:27 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1513,3 +1513,36 @@ int main() {
     <<median(nums1,nums2,m,n);
     return 0;
 }
+
+
+// 46
+// Valid Parentheses
+// Input: s = "()[]{}"
+// Output: true
+// Input: s = "(]"
+// Output: false
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isValid(string s) {
+        stack<char>st; 
+        for(auto it: s) {
+            if(it=='(' || it=='{' || it == '[') st.push(it); 
+            else {
+                if(st.size() == 0) return false; 
+                char ch = st.top(); 
+                st.pop(); 
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) continue;
+                else return false;
+            }
+        }
+        return st.empty(); 
+    }
+int main()
+{
+    string s="()[{}()]";
+    if(isValid(s))
+    cout<<"True"<<endl;
+    else
+    cout<<"False"<<endl;
+}
