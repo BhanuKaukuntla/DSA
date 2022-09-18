4d59cf0a3d9cbc455ca722ed6a40b99d31dfe051
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-18 22:32:15 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1708,3 +1708,97 @@ public:
         return ans; 
     }
 };
+
+// 49
+// Binary Tree Zigzag Level Order Traversal
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[20,9],[15,7]]
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+  public:
+    int val;
+  Node * left, * right;
+};
+
+vector < vector < int >> zigzagLevelOrder(Node * root) {
+  vector < vector < int >> result;
+  if (root == NULL) {
+    return result;
+  }
+
+  queue < Node * > nodesQueue;
+  nodesQueue.push(root);
+  bool leftToRight = true;
+
+  while (!nodesQueue.empty()) {
+    int size = nodesQueue.size();
+    vector < int > row(size);
+    for (int i = 0; i < size; i++) {
+      Node * node = nodesQueue.front();
+      nodesQueue.pop();
+
+      // find position to fill node's value
+      int index = (leftToRight) ? i : (size - 1 - i);
+
+      row[index] = node -> val;
+      if (node -> left) {
+        nodesQueue.push(node -> left);
+      }
+      if (node -> right) {
+        nodesQueue.push(node -> right);
+      }
+    }
+    // after this level
+    leftToRight = !leftToRight;
+    result.push_back(row);
+  }
+  return result;
+}
+Node * newNode(int data) {
+  Node * node = new Node;
+  node -> val = data;
+  node -> left = NULL;
+  node -> right = NULL;
+  return node;
+}
+int main() {
+  int i, j;
+  Node * root = newNode(3);
+  root -> left = newNode(9);
+  root -> right = newNode(20);
+  root -> right -> left = newNode(15);
+  root -> right -> right = newNode(7);
+  vector < vector < int >> ans;
+  ans = zigzagLevelOrder(root);
+  cout << "Zig Zag Traversal of Binary Tree" << endl;
+  for (i = 0; i < ans.size(); i++) {
+    for (j = 0; j < ans[i].size(); j++) {
+      cout << ans[i][j] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
+
+
+// 50
+// Maximum Depth of Binary Tree
+// Input: root = [3,9,20,null,null,15,7]
+// Output: 3
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0; 
+        
+        int lh = maxDepth(root->left); 
+        int rh = maxDepth(root->right); 
+        
+        return 1 + max(lh, rh); 
+    }
+};
