e77918f40157ea356fa4210d6d4d405409fdb699
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-18 10:38:20 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1225,10 +1225,48 @@ public:
 };
 
 
-// 39
+
+// 39 part-1
 // Contains Duplicate
 // Input: nums = [1,2,3,1]
 // Output: true
+
+#include<bits/stdc++.h>
+using namespace std;
+    
+bool containsDuplicate(vector<int> nums) {
+ 
+    unordered_set<int> set;
+    
+    for(int i = 0; i<nums.size();i++){
+        set.insert(nums[i]);
+    }
+    
+    // if it contains duplicate then the size of 
+    // set will not equal to the length of the array
+    if(set.size() < nums.size())
+        return true;
+    
+    return false;
+}
+    
+int main () {
+	    
+    vector<int> nums {1, 2, 3, 1};
+    
+    bool res = containsDuplicate(nums);
+    
+    // printing the result
+    if(res== 1)
+        cout<<"true"<<endl;
+    else
+        cout<<"false"<<endl;
+}
+
+// 39 part-2
+// Find Duplicate
+// Input: nums = [1,2,3,1]
+// Output: 1
 
 #include<bits/stdc++.h>
 
