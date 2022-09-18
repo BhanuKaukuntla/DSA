b2e859620c0cb40c9541f770da75a48039a6dfa9
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-09-18 21:04:46 +0530

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1674,3 +1674,37 @@ int main() {
 
   return 0;
 }
+
+
+
+// 48
+// Binary Tree Level Order Traversal
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+
+class Solution {
+public:
+    vector<int> levelOrder(TreeNode* root) {
+        vector<int> ans; 
+        
+        if(root == NULL) 
+            return ans; 
+            
+        queue<TreeNode*> q; 
+        q.push(root); 
+        
+        while(!q.empty()) {
+           
+            TreeNode *temp = q.front(); 
+            q.pop(); 
+            
+            if(temp->left != NULL) 
+                q.push(temp->left); 
+            if(temp->right != NULL) 
+                q.push(temp->right); 
+                
+            ans.push_back(temp->val); 
+        }
+        return ans; 
+    }
+};
