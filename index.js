b20e391e36fe85e3894b22d439f42b8f19d29500
BhanuKaukuntla <59928614+BhanuKaukuntla@users.noreply.github.com>
2022-10-05 16:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -359,6 +359,44 @@ function moveZeroes(nums) {
     return lps;
 }
   
+  or 
+  
+function longestPalindrome(str)
+{
+    let n = str.length; // calculating size of string
+    if (n < 2)
+        return str; // if string is empty then size will be 0.
+                // if n==1 then, answer will be 1(single
+                // character will always palindrome)
+ 
+    let maxLength = 1,start=0;
+    let low, high;
+    for (let i = 0; i < n; i++) {
+        low = i - 1;
+        high = i + 1;
+        while ( high < n && str[high] == str[i]) //increment 'high'                               
+            high++;
+     
+        while ( low >= 0 && str[low] == str[i]) // decrement 'low'               
+            low--;
+     
+        while (low >= 0 && high < n && str[low] == str[high]){
+            low--;
+            high++;
+        }
+ 
+        let length = high - low - 1;
+        if (maxLength < length) {
+            maxLength = length;
+            start=low+1;
+        }
+    }
+     
+    // document.write("Longest palindrome substring is: ");
+    // document.write(str.substring(start,maxLength+start));
+    return str.substring(start,maxLength+start);
+}
+  
 // 14
 // Javascript program to add two numbers
 // represented by linked list
